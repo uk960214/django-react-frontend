a219e117c92b5e1fff9c5f9c6587d53052209957
koowoy <uk960214@gmail.com>
2021-07-22 20:22:07 +0900

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const axios = require('axios').default;
 
+class App extends Component {
+  state = {
+    posts: []
+  };
+
+  async componentDidMount () {
+    try {
+      // const res = await fetch('http://127.0.0.1:8000/api/');
+      // const posts = await res.json();
+      const res = await axios.get('http://127.0.0.1:8000/api/');
+      const posts = await res.data
+      this.setState({
+        posts
+      });
+    } catch (e) {
+      console.log(e);
+    }
+  }
+
+  render() {
+    return (
+      <div className="App">
+        {this.state.posts.map(item => (
+          <div key={item.id}>
+            <h1>{item.title}</h1>
+            <span>{item.content}</span>
+          </div>
+        ))}
+        <button onClick={() => this.sendPost()}>send</button>
+      </div>
+    );
+  }
+}
 export default App;
